f9494b537f63433ea810a90de02808f10afd1450
marinavalenciamoreno <maylu_enf@hotmail.com>
2020-05-25 19:02:41 +0200

    Comit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { FechaactualComponent } from './fechaactual/fechaactual.component';
+import { CopyrightComponent } from './copyright/copyright.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, FechaactualComponent, CopyrightComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
